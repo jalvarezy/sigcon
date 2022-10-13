daedc519b9f4a04708df3b5ff8d6a62a4da8b3a3
Jorge Alvarez <jalvarez.y.89@gmail.com>
2022-10-12 22:56:37 -0500

    Primer Avance Proyecto Sigcon

---
+++ backend/src/models/rol.model.ts
@@ -0,0 +1,34 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Rol extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre_rol: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  rol_id: string;
+
+
+  constructor(data?: Partial<Rol>) {
+    super(data);
+  }
+}
+
+export interface RolRelations {
+  // describe navigational properties here
+}
+
+export type RolWithRelations = Rol & RolRelations;
